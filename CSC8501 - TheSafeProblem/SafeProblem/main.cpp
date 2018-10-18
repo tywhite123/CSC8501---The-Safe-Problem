87db3913b827d852f36ea16e40d3ddef954c3207
Tyler <tywhitehead@hotmail.co.uk>
2018-10-18 12:54:31 +0100

    Nicer printing and got multi safe working

--- CSC8501 - TheSafeProblem/SafeProblem/main.cpp
+++ CSC8501 - TheSafeProblem/SafeProblem/main.cpp
@@ -9,6 +9,7 @@ int main() {
 
 	HashFunctions h;
 
+	int print = 0;
 
 	int uhfA, uhfB, uhfC, uhfD;
 	int lhfA, lhfB, lhfC, lhfD;
@@ -45,11 +46,15 @@ int main() {
 
 	std::cout << "PHF:\t" << phfA << ", " << phfB << ", " << phfC << ", " << phfD << std::endl;
 
+	std::cout << "\n----------------------------------------------------------------------------\n\n";
+
 	int noOfS;
 	std::cin >> noOfS;
 
 	for (int i = 0; i < noOfS; ++i) {
-		std::cout << "Solution " << i + 1 << std::endl << std::endl;
+
+		bool isValid = true;
+		/*std::cout << "Solution " << i + 1 << std::endl << std::endl;*/
 
 		int a, b, c, d;
 		/*std::cout << "Please Enter Root: " << std::endl;
@@ -65,67 +70,111 @@ int main() {
 		b = i % 1000 / 100;
 		a = i / 1000;
 
-		std::cout << "ROOT:\t" << a << b << c << d << std::endl;
+		//std::cout << "ROOT:\t" << a << b << c << d << std::endl;
 
 
 		Lock l(a, b, c, d);
 
-		h.hashRoot(l.getLock());
+		//h.hashRoot(l.getLock());
 
-		Dial CN[4];
-		Dial LN[4];
-		Dial HN[4];
+		Dial CN[5][4];
+		Dial LN[5][4];
+		Dial HN[5][4];
 
-		CN[0] = l.getAt(0) + uhfA;
-		CN[1] = l.getAt(1) + uhfB;
-		CN[2] = l.getAt(2) + uhfC;
-		CN[3] = l.getAt(3) + uhfD;
+		for (int j = 0; j < 5; ++j) {
 
-		std::cout << "CN:\t";
-		for (int i = 0; i < 4; i++)
-			std::cout << CN[i].getEntry();
-		std::cout << std::endl;
+			if (j == 0) {
+
+				CN[j][0] = l.getAt(0) + uhfA;
+				CN[j][1] = l.getAt(1) + uhfB;
+				CN[j][2] = l.getAt(2) + uhfC;
+				CN[j][3] = l.getAt(3) + uhfD;
+			}
+			else
+			{
+				CN[j][0] = HN[j-1][0] + lhfA;
+				CN[j][1] = HN[j-1][1] + lhfB;
+				CN[j][2] = HN[j-1][2] + lhfC;
+				CN[j][3] = HN[j-1][3] + lhfD;
+			}
+
+			//std::cout << "CN" << j << ":";
+			//for (int i = 0; i < 4; i++)
+			//	std::cout << CN[j][i].getEntry();
+			//std::cout << "\t"; //<< std::endl;
 
 
-		LN[0] = CN[0] + lhfA;
-		LN[1] = CN[1] + lhfB;
-		LN[2] = CN[2] + lhfC;
-		LN[3] = CN[3] + lhfD;
+			LN[j][0] = CN[j][0] + lhfA;
+			LN[j][1] = CN[j][1] + lhfB;
+			LN[j][2] = CN[j][2] + lhfC;
+			LN[j][3] = CN[j][3] + lhfD;
+						 
+			//std::cout << "LN" << j << ":";
+			//for (int i = 0; i < 4; i++)
+			//	std::cout << LN[j][i].getEntry();
+			//std::cout << "\t"; //<< std::endl;
 
-		std::cout << "LN:\t";
-		for (int i = 0; i < 4; i++)
-			std::cout << LN[i].getEntry();
-		std::cout << std::endl;
 
 
+			HN[j][0] = LN[j][0] + phfA;
+			HN[j][1] = LN[j][1] + phfB;
+			HN[j][2] = LN[j][2] + phfC;
+			HN[j][3] = LN[j][3] + phfD;
 
-		HN[0] = LN[0] + phfA;
-		HN[1] = LN[1] + phfB;
-		HN[2] = LN[2] + phfC;
-		HN[3] = LN[3] + phfD;
+			/*std::cout << "HN" << j << ":";
+			for (int i = 0; i < 4; i++)
+				std::cout << HN[j][i].getEntry();
+			std::cout << std::endl;*/
 
-		std::cout << "HN:\t";
-		for (int i = 0; i < 4; i++)
-			std::cout << HN[i].getEntry();
-		std::cout << std::endl;
-
-		bool isValid = true;
-		//How the check with work
-		for (int i = 0; i < 3 /*|| isValid*/; ++i) {
-			for (int j = i + 1; j < 4 /*|| isValid*/; ++j) {
-				//std::cout << "Comparing: " << CN[i].getEntry() << " & " << CN[j].getEntry() << std::endl;
-				if (CN[i].getEntry() == CN[j].getEntry()) {
-					std::cout << "They are the same value, so invalid CN" << std::endl;
-					isValid = false;
-					break;
+			
+			//How the check with work
+			for (int i = 0; i < 3 /*|| isValid*/; ++i) {
+				for (int k = i + 1; k < 4 /*|| isValid*/; ++k) {
+					//std::cout << "Comparing: " << CN[i].getEntry() << " & " << CN[j].getEntry() << std::endl;
+					if (CN[j][i].getEntry() == CN[j][k].getEntry()) {
+						//std::cout << "They are the same value, so invalid CN" << std::endl;
+						isValid = false;
+						break;
+					}
 				}
 			}
+
 		}
 
-		std::cout << "\n----------------------------------------------------------------------------\n\n";
+		if (isValid) {
+
+			++print;
+			std::cout << "\tSolution " << print << std::endl << std::endl;
+
+			std::cout << "\tROOT:\t" << a << b << c << d << std::endl;
+
+			for (int j = 0; j < 5; ++j) {
+				std::cout << "\tCN" << j << ":";
+				for (int i = 0; i < 4; i++)
+					std::cout << CN[j][i].getEntry();
+				std::cout << "\t"; //<< std::endl;
+
+				std::cout << "LN" << j << ":";
+				for (int i = 0; i < 4; i++)
+					std::cout << LN[j][i].getEntry();
+				std::cout << "\t"; //<< std::endl;
+
+				std::cout << "HN" << j << ":";
+				for (int i = 0; i < 4; i++)
+					std::cout << HN[j][i].getEntry();
+				std::cout << std::endl;
+			}
+
+			
+
+			std::cout << "\n----------------------------------------------------------------------------\n\n";
+
+		}
+
+		/*std::cout << "\n----------------------------------------------------------------------------\n\n";*/
 	}
 
-
+	std::cout << "Total Number of Solutions: " << print;
 	
 	return 0;
 }
